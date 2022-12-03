1ff1a2f860bb942fa9ae97cd8a50bfa7790e8823
Patrick Verkaik <verkaik.patrick@euronet.nl>
2022-12-04 00:12:25 +0100

    day 3; shorter

--- day3/day3p2.js
+++ day3/day3p2.js
@@ -1,4 +1,3 @@
-const { group } = require('console');
 const fs = require('fs');
 
 const content = fs.readFileSync('day3input.txt', 'utf-8').split(/\r?\n/);
@@ -7,37 +6,22 @@ main();
 
 function main() {
 
-    let groupId = 0;
-    const groups = [content.length]
-
-    groups[groupId] = [];
-    for (x in content) {
-
-        groups[groupId].push(content[x]);
-        if ((x + 1) % 3 === 0) {
-            // console.log('every third')
-            groupId++;
-            groups[groupId] = [];
-        }
-    }
-
-    groups.pop();
-
-    console.log(groups);
-
     let sum = 0;
-    groups.forEach(group => {
-        const occurrences1 = getOccurrences(group[0]);
-        const occurrences2 = getOccurrences(group[1]);
-        const occurrences3 = getOccurrences(group[2]);
+    for (let x = 0; x < (content.length / 3); x = x + 1) {
 
-        const occurrenceInAll = Object.keys(occurrences1)
-            .find(character => occurrences2[character] !== undefined &&
-                occurrences3[character] !== undefined);
+        let occurrences1 = getOccurrences(content[(x * 3)]);
+        let occurrences2 = getOccurrences(content[(x * 3 + 1)]);
+        let occurrences3 = getOccurrences(content[(x * 3 + 2)]);
+
+        let occurrenceInAll = Object.keys(occurrences1)
+            .find(character => {
+                return occurrences2[character] !== undefined &&
+                    occurrences3[character] !== undefined
+            });
         console.log(occurrenceInAll);
         console.log(getCharCodeValue(occurrenceInAll.charCodeAt(0)));
         sum += getCharCodeValue(occurrenceInAll.charCodeAt(0))
-    });
+    };
 
 
     console.log(sum);
